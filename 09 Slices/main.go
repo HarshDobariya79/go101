a0d1d50e73ae22bb2505a742ab2cb602bdbb3048
silverskull <silverskull@23.10>
2024-01-09 14:49:08 +0530

    Slices

---
+++ 09 Slices/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var nums = []int{}
+
+	nums = append(nums, 1, 5, 3)
+
+	fmt.Println(nums)
+
+	nums = append(nums[1:], 6, 5)
+
+	fmt.Println(nums)
+
+	var items = make([]int, 4)
+
+	fmt.Println(items)
+
+	items = append(items, 3, 6, 3)
+
+	fmt.Println(items)
+
+	fmt.Println(sort.IntsAreSorted(items))
+
+	sort.Ints(items)
+
+	fmt.Println(items)
+
+	fmt.Println(sort.IntsAreSorted(items))
+}
